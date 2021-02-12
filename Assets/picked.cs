87e0f5a57bbf5c7ce98de32eb75a8324b789526e
Hongke <luohongke2000@gmail.com>
2021-02-12 09:57:33 +0800

    pickup (not completely done)

---
+++ Assets/picked.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class picked : MonoBehaviour
+{
+    public bool approached = false;
+    public Behaviour halo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (approached)
+        {
+            halo.enabled = true;
+        }
+        else
+        {
+            halo.enabled = false;
+        }
+    }
+}
---
+++ Assets/throwAway.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwAway : MonoBehaviour
+{
+    public bool picked = false;
+    public GameObject thisBall;
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!picked && thisBall != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                picked = true; 
+                thisBall.transform.position = new Vector3(transform.position.x+0.5f, transform.position.y, transform.position.z+0.5f);
+                thisBall.transform.parent = cam.transform;
+                Rigidbody rb = thisBall.GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+                
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "pickup")
+        {
+            picked p = other.gameObject.GetComponent<picked>();
+            p.approached = true;
+            thisBall = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "pickup")
+        {
+            picked p = other.gameObject.GetComponent<picked>();
+            p.approached = false;
+            thisBall = null;
+        }
+    }
+
+}
