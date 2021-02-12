d00bee9d29f568e313fac064f94de8401b77a0dd
Ina Joe <ij2049@nyu.edu>
2021-02-12 10:56:28 +0900

    trigger door open

---
+++ Assets/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    float throwForce = 600;
+    Vector3 objectPosition;
+    float distance;
+
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject temParent;
+    public bool isHolding = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if isholding
+
+    }
+
+
+}
